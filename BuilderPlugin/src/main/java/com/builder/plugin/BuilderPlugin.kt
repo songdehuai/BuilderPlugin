60d94366743d4a9179ad9ff42436098d4a9ef0cb
Doracoin <doracoin@foxmail.com>
2020-11-26 23:46:48 +0800

    TestCodes.

--- BuilderPlugin/src/main/java/com/builder/plugin/BuilderPlugin.kt
+++ BuilderPlugin/src/main/java/com/builder/plugin/BuilderPlugin.kt
@@ -2,6 +2,7 @@ package com.builder.plugin
 
 import com.builder.plugin.task.ConfigTask
 import com.builder.plugin.task.InjectTask
+import com.builder.plugin.task.Up2Server
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
@@ -10,6 +11,7 @@ class BuilderPlugin : Plugin<Project> {
     override fun apply(project: Project) {
         project.tasks.create("InjectTask", InjectTask::class.java).group = "builder"
         project.tasks.create("ConfigTask", ConfigTask::class.java).group = "builder"
+        project.tasks.create("Up2Server", Up2Server::class.java).group = "builder"
     }
 
 }
